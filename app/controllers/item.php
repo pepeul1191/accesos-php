f7f3a4026e34fe4847b7b37aa91e8e0605cd8dcd
Pepe Valdivia <jvaldivia@softweb.pe>
2017-05-19 17:22:55 -0500

    Correción de las variables del algoritmo de item/menu_submodulos

--- app/controllers/item.php
+++ app/controllers/item.php
@@ -54,19 +54,16 @@ class Controller_Item extends Controller
 		$rpta = [];
 
 		for($i = 0; $i < count($items); $i++){
-			//print_r($items[$i]);echo "<br>";
 			if(!in_array($items[$i]['subtitulo'] , $temp_subtitulos, true)){
 				array_push($temp_subtitulos, $items[$i]['subtitulo']);
-				$temp[0] = array('item' => $items[$i]['item'], 'url' => $items[$i]['url']);
-		       $temp_items[$items[$i]['subtitulo']] = $temp;
+		       $temp_item[0] = array('item' => $items[$i]['item'], 'url' => $items[$i]['url']);
+		       $temp_items[$items[$i]['subtitulo']] = $temp_item;
 		    }else{
-		    	//var_dump($temp_items);exit();
-		    	$temp = $temp_items[$items[$i]['subtitulo']];
+		       	$temp = $temp_items[$items[$i]['subtitulo']];
 		    	array_push($temp, array('item' => $items[$i]['item'], 'url' => $items[$i]['url']));
 		    	$temp_items[$items[$i]['subtitulo']] = $temp;
 		    }
 		}
-
 		foreach ($temp_subtitulos as $subtitulo) {
 			#print_r($subtitulo);
 			$temp = array('subtitulo' => $subtitulo, 'items' => $temp_items[$subtitulo]);
